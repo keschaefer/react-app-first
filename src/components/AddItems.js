647d5b516f46209e26874021d0fd6714cc5cbf67
keschaefer <keschaefer@gmail.com>
2018-10-10 18:58:31 -0600

    added back AddItems Component

---
+++ src/components/AddItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddItems = (props) => {
+    return props.Products.map(i => {
+        return (
+        <option key = {i.name}>{i.name}</option>
+        )})
+      }
+
+export default AddItems

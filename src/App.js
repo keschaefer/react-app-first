e9859f3615ab3c9f6855efb1ff28ba812bbba30a
keschaefer <keschaefer@gmail.com>
2018-10-10 19:32:50 -0600

    import AddItems component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CartHeader from './components/CartHeader.js';
 import CartFooter from './components/CartFooter.js';
 import CartItems from './components/CartItems.js';
+import AddItem from './components/AddItems';
 
 import './App.css';
 
@@ -16,6 +17,17 @@ class App extends Component {
       { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
       { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
       ],
+      Products: [
+        { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+        { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+        { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+        { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+        { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+        { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+        { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+        { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+        { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+      ],
     };
   }
 
@@ -25,6 +37,19 @@ class App extends Component {
       <div>
       <CartHeader />
       <CartItems CartItemsList = {this.state.CartItemsList} />
+      <form className="form-group">
+        <p>Total $</p>
+        <label>Quantity</label>
+            <input type="number" className="form-control" id="exampleFormControlTextarea1" rows="3"></input>
+        <div id="product-menu">
+            <label>Products</label>
+            <select>
+                <option>Select an Option...</option>
+                <AddItem Products = {this.state.Products} />
+            </select>
+        </div> 
+        <button id="submit">Submit</button>
+      </form>
       <CartFooter copyright = {this.state.copyright} year = {this.state.year}/> 
       </div>
     );
